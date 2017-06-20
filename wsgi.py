994d95fa6c42cae69f7382bcb2bf73136111f4bc
Andrei Hajdukewycz <sancus@off.net>
2017-06-20 10:07:32 -0700

    prevent proxy from caching language redirects, but allow browser to do so

--- wsgi.py
+++ wsgi.py
@@ -74,9 +74,7 @@ def application(environ, start_response):
 
     start_response('302 Found',
     [('Content-type', 'text/html; charset=utf-8' ),
-    ('Cache-Control', 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'),
-    ('Pragma', 'no-cache'),
-    ('Expires', '01 Jan 1970 00:00:00 GMT' ),
+    ('Cache-Control', 'private, s-maxage=0, max-age=604800'),
     ('Vary', 'Accept-Language'),
     ('Location', location)
     ])
