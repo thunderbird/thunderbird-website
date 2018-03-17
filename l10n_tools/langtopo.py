89c944cb753d5ffeda40d3ded6498f81f9b86455
Andrei Hajdukewycz <sancus@off.net>
2018-03-17 16:02:55 -0700

    Fix broken strings.

--- l10n_tools/langtopo.py
+++ l10n_tools/langtopo.py
@@ -8,14 +8,13 @@ import settings
 import translate
 
 for lang in settings.PROD_LANGUAGES:
-    if lang != 'en-US':
+    if lang != 'en-US' and lang != 'it' and lang != 'pl':
         popath = 'locale/{0}/LC_MESSAGES/messages.po'.format(lang.replace('-','_'))
         po = polib.pofile(popath)
-        translator = translate.Translation(lang, ['thunderbird/start/release', 'thunderbird/index', 'thunderbird/features', 'thunderbird/channel', 'main', 'download_button'])
+        translator = translate.Translation(lang, ['thunderbird/start/release'])
         for entry in po:
-            if not entry.msgstr:
-                translation = translator.gettext(entry.msgid)
-                if translation not in entry.msgid:
-                    entry.msgstr=polib.escape(translation)
+            if entry.msgid=='Welcome to <span>Thunderbird</span>' or entry.msgid=='Donate to Thunderbird' or entry.msgid=='Contribute to Thunderbird':
+                if entry.msgstr==entry.msgid:
+                    entry.msgstr=''
         print "{0} language po file edited.\n".format(lang)
         po.save()
