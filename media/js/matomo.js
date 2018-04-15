c729731e61ff112510c3d00e0ec2298ecaa63e43
Andrei Hajdukewycz <sancus@off.net>
2018-04-15 13:29:46 -0700

    Add Matomo analytics to website.

---
+++ media/js/matomo.js
@@ -0,0 +1,8 @@
+var u="//thunderbird.innocraft.cloud/";
+var _paq = _paq || [];  
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', '1']);
+_paq.push(["setCookieDomain", "*.thunderbird.net"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
