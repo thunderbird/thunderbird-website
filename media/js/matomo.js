0cdaa5ffedda1de33ff9ff7ddb2f98d1fe44f739
Andrei Hajdukewycz <sancus@off.net>
2018-08-09 10:40:12 -0700

    Fix download link tracking.

--- media/js/matomo.js
+++ media/js/matomo.js
@@ -1,8 +1,9 @@
 var u="//thunderbird.innocraft.cloud/";
-var _paq = _paq || [];  
+var _paq = _paq || [];
 _paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 _paq.push(['setTrackerUrl', u+'piwik.php']);
 _paq.push(['setSiteId', '1']);
 _paq.push(["setCookieDomain", "*.thunderbird.net"]);
+_paq.push(['setDownloadClasses', "download-link"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
