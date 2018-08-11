c4ebe95b1cf5fe01de6ec8288b8f95463a0bd6a2
Andrei Hajdukewycz <sancus@off.net>
2018-08-11 10:12:04 -0700

    Remove donation redirect on download.

--- assets/js/common/mozilla-utils.js
+++ assets/js/common/mozilla-utils.js
@@ -36,7 +36,6 @@ if (typeof Mozilla === 'undefined') {
             var $el = $(this);
             var donate_link = $el.data('donate-link')
             $el.click(function() {
-                window.open(donate_link)
                 Utils.triggerIEDownload($el.data('direct-link'));
             });
         });
