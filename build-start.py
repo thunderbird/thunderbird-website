c0b41afd4587e2b59714540d55b534bf3012a545
Andrei Hajdukewycz <sancus@off.net>
2019-04-21 22:59:49 -0700

    Add asset handling to site builder.

--- build-start.py
+++ build-start.py
@@ -10,18 +10,8 @@ searchpath = 'start-page'
 staticpath = 'start-page/_media'
 # path to render the finished site to
 renderpath = 'site'
-# path to compile CSS to
-cssout = renderpath+'/media/css'
 
-def build_assets():
-    env = webassets.Environment(load_path=[settings.ASSETS], directory=cssout, url=settings.MEDIA_URL, cache=False, manifest=False)
-    start_css = webassets.Bundle('less/sandstone/fonts.less', 'less/thunderbird/start.less',  filters='less', output='start-style.css')
-    env.register('start-style', start_css)
-    env['start-style'].urls()
-    shutil.rmtree(renderpath+'/media', ignore_errors=True)
-    shutil.copytree(staticpath, renderpath+'/media')
+css_bundles = {'start-style': ['less/sandstone/fonts.less', 'less/thunderbird/start.less']}
 
-
-site = builder.Site(settings.PROD_LANGUAGES, searchpath, renderpath)
+site = builder.Site(settings.PROD_LANGUAGES, searchpath, renderpath, staticpath, css_bundles)
 site.build_site()
-build_assets()
--- builder.py
+++ builder.py
@@ -10,12 +10,15 @@ from jinja2 import Environment, FileSystemLoader
 extensions = ['jinja2.ext.i18n']
 
 class Site(object):
-    def __init__(self, languages, searchpath, renderpath):
+    def __init__(self, languages, searchpath, renderpath, staticpath, css_bundles):
         self.languages = languages
         self.lang = languages[0]
         self.context = {}
         self.searchpath = searchpath
         self.renderpath = renderpath
+        self.staticpath = staticpath
+        self.cssout = renderpath+'/media/css'
+        self.css_bundles = css_bundles
 
     def _text_dir(self):
         textdir = 'ltr'
@@ -30,6 +33,15 @@ class Site(object):
     def _setup_env(self):
         load = FileSystemLoader(self.searchpath)
         self._env = Environment(loader=load, extensions=extensions)
+
+    def build_assets(self):
+        env = webassets.Environment(load_path=[settings.ASSETS], directory=self.cssout, url=settings.MEDIA_URL, cache=False, manifest=False)
+        for k, v in self.css_bundles.iteritems():
+            reg = webassets.Bundle(*v, filters='less', output=k+'.css')
+            env.register(k, reg)
+            env[k].urls()
+        shutil.rmtree(self.renderpath+'/media', ignore_errors=True)
+        shutil.copytree(self.staticpath, self.renderpath+'/media')
 
     def render(self):
         outpath = os.path.join(self.renderpath, self.lang)
@@ -52,3 +64,4 @@ class Site(object):
             self._env.install_gettext_translations(translator)
             self._env.globals.update(l10n_css=translator.l10n_css, **helper.contextfunctions)
             self.render()
+        self.build_assets()
