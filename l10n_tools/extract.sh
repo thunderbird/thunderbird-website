98fd433391726aeff48c9bb9077c11bbd8dca5a6
Andrei Hajdukewycz <sancus@off.net>
2019-11-28 14:25:39 -0800

    Fix l10n extraction for redesign.

--- l10n_tools/extract.sh
+++ l10n_tools/extract.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 cd ../website/_includes
-mv -t .. base-resp.html download-button.html lang_switcher.html site-footer.html
+mv -t .. base-resp.html download-button.html lang_switcher.html site-footer.html site-prefooter.html donate-mailing-list.html
 cd ../..
 pybabel extract -F babel.cfg -o locale/templates/LC_MESSAGES/messages.pot .
 cd website
-mv -t _includes base-resp.html download-button.html lang_switcher.html site-footer.html
+mv -t _includes base-resp.html download-button.html lang_switcher.html site-footer.html site-prefooter.html donate-mailing-list.html
 cd ../l10n_tools
 bash merge.sh
 python linelength.py
