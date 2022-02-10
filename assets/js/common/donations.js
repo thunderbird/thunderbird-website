6dfe850a316558e312413662b365b4882e85b3ea
Andrei Hajdukewycz <sancus@off.net>
2022-02-10 14:15:41 -0800

    Add very basic front-end.

---
+++ assets/js/common/donations.js
@@ -0,0 +1,68 @@
+// Create namespace
+if (typeof Mozilla === 'undefined') {
+    var Mozilla = {};
+}
+
+(function() {
+    'use strict';
+
+    var Donation = {};
+    var braintree_URL = 'https://chaos.thunderbird.net'
+
+    Donation.BuildForm = function(client_token) {
+        var button = document.querySelector('#submit-button');
+        braintree.dropin.create({
+            // Insert your tokenization key here
+            authorization: client_token,
+            container: '#dropin-container'
+        }, function(createErr, instance) {
+            $('#submit-button').show();
+            button.addEventListener('click', function() {
+                instance.requestPaymentMethod(function(requestPaymentMethodErr, payload) {
+                    // When the user clicks on the 'Submit payment' button this code will send the
+                    // encrypted payment information in a variable called a payment method nonce
+                    $.ajax({
+                        type: 'POST',
+                        url: braintree_URL +'/checkout',
+                        data: {
+                            'payment_method_nonce': payload.nonce,
+                            'amount': 10
+                        }
+                    }).done(function(result) {
+                        // Tear down the Drop-in UI
+                        instance.teardown(function(teardownErr) {
+                            if (teardownErr) {
+                                console.error('Could not tear down Drop-in UI!');
+                            } else {
+                                console.info('Drop-in UI has been torn down!');
+                                // Remove the 'Submit payment' button
+                                $('#submit-button').remove();
+                            }
+                        });
+
+                        if (result.success) {
+                            $('#checkout-message').html('<h1>'+result.message+'</h1><p>Refresh to try again.</p>');
+                        } else {
+                            console.log(result);
+                            $('#checkout-message').html('<h1>Error:'+result.message+'</h1><p>Check your console.</p>');
+                        }
+                    });
+                });
+            });
+        });
+    };
+
+    Donation.InitForm = function() {
+        $.ajax({
+            type: 'GET',
+            url: braintree_URL + '/client_token'
+            }).done(function(result) {
+                if (result) {
+                    Donation.BuildForm(result)
+                }
+            });
+    };
+
+    window.Mozilla.Donation = Donation;
+
+})();
