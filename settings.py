5fbad6e6d1cc4c4f4111fcd8c179ee0b861900f4
Melissa Autumn <melissa@thunderbird.net>
2023-10-19 09:44:47 -0700

    Implemented the new donor help form. (Fixes #508)

--- settings.py
+++ settings.py
@@ -163,6 +163,8 @@ URL_MAPPINGS = {
     'legal.index': 'https://www.mozilla.org/en-US/about/legal/terms/mozilla/',
     'legal.infringement': 'https://www.mozilla.org/en-US/about/legal/report-infringement/',
     'mozorg.home': 'https://www.mozilla.org/',
+    'mozorg.connect': 'https://connect.mozilla.org/',
+    'mozorg.connect.tb': 'https://connect.mozilla.org/t5/ideas/idb-p/ideas/label-name/thunderbird',
     'mzla.blog-post': 'https://blog.thunderbird.net/2020/01/thunderbirds-new-home/',
     'privacy': 'https://www.mozilla.org/privacy/websites/',
     'privacy.notices.websites': 'https://www.mozilla.org/privacy/websites/#data-tools',
