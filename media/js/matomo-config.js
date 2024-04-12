d35d8d1479687d7a05fcd6f6b920319bde76f6da
Melissa Autumn <melissa@thunderbird.net>
2024-04-12 11:01:15 -0700

    Update matomo js lib

---
+++ media/js/matomo-config.js
@@ -0,0 +1,12 @@
+var u="//thunderbird.innocraft.cloud/";
+var _paq = _paq || [];
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
+_paq.push(['setTrackerUrl', u+'piwik.php']);
+_paq.push(['setSiteId', '1']);
+_paq.push(["setCookieDomain", "*.thunderbird.net"]);
+_paq.push(['setDownloadClasses', "matomo-track-download"]);
+_paq.push(['trackPageView']);
+_paq.push(['enableLinkTracking']);
+if (window.Mozilla.ABTest) {
+  window.Mozilla.ABTest.Track();
+}
