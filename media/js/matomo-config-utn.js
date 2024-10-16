af001375a986fe8a0e227bdd8a9679191b04cd52
Melissa Autumn <melissa@thunderbird.net>
2024-10-16 13:23:00 -0700

    Update matomo-config-utn to use a newer local matomo.js file.

--- media/js/matomo-config-utn.js
+++ media/js/matomo-config-utn.js
@@ -1,17 +1,13 @@
 var _paq = window._paq = window._paq || [];
+var u="https://thunderbird.innocraft.cloud/";
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
-(function() {
-  var u="https://thunderbird.innocraft.cloud/";
-  _paq.push(['setTrackerUrl', u+'matomo.php']);
-  _paq.push(['setSiteId', `${window._utn.matomoSiteId || 3}`]);
-  _paq.push(["setDocumentTitle", window.location.hostname + "/" + document.title]);
-
-  var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
-  g.async=true; g.src='https://cdn.matomo.cloud/thunderbird.innocraft.cloud/matomo.js'; s.parentNode.insertBefore(g,s);
-})();
-
+_paq.push(['setTrackerUrl', u+'matomo.php']);
+_paq.push(["setCookieDomain", "*.thunderbird.net"]);
+_paq.push(['setDownloadClasses', "matomo-track-download"]);
+_paq.push(['setSiteId', `${window._utn.matomoSiteId || 3}`]);
+_paq.push(["setDocumentTitle", window.location.hostname + "/" + document.title]);
 if (window.Mozilla.ABTest) {
   window.Mozilla.ABTest.Track();
 }
