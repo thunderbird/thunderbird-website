c33a5fe99aa27fcd91fb3e4384e28f4e3029fde8
Melissa Autumn <melissa@thunderbird.net>
2024-10-30 08:34:33 -0700

    Update updates tracking code

--- media/js/matomo-config-utn.js
+++ media/js/matomo-config-utn.js
@@ -2,12 +2,14 @@ var _paq = window._paq = window._paq || [];
 var u="https://thunderbird.innocraft.cloud/";
 /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
 _paq.push(['trackPageView']);
+// Disable cookies needs to be set _after_ trackPageView for it to actually work.
+_paq.push(["disableCookies"]);
 _paq.push(['enableLinkTracking']);
 _paq.push(['setTrackerUrl', u+'matomo.php']);
 _paq.push(["setCookieDomain", "*.thunderbird.net"]);
 _paq.push(['setDownloadClasses', "matomo-track-download"]);
 _paq.push(['setSiteId', `${window._utn.matomoSiteId || 3}`]);
-_paq.push(["setDocumentTitle", window.location.hostname + "/" + document.title]);
+_paq.push(["setDocumentTitle", document.domain + "/" + document.title]);
 if (window.Mozilla.ABTest) {
   window.Mozilla.ABTest.Track();
 }
--- media/js/matomo-config.js
+++ media/js/matomo-config.js
@@ -7,6 +7,8 @@ _paq.push(["setCookieDomain", "*.thunderbird.net"]);
 _paq.push(['setDownloadClasses', "matomo-track-download"]);
 _paq.push(['trackPageView']);
 _paq.push(['enableLinkTracking']);
+// Disable cookies needs to be set _after_ trackPageView for it to actually work.
+_paq.push(["disableCookies"]);
 if (window.Mozilla.ABTest) {
   window.Mozilla.ABTest.Track();
 }
