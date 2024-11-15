00e79ce4ee664b71ab7c3e558ac1670e8c4666b1
Malini Das <malini@thunderbird.net>
2024-11-15 14:39:13 -0500

    correct author, copyright and titles

--- docs/conf.py
+++ docs/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Thunderbird Websites'
+copyright = '2024, Mozilla'
+author = 'Mozilla'
 
 release = '0.1'
 version = '0.1.0'
