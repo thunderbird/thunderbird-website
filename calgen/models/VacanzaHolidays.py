fdaf8779874d752a53ea01b2c17a20d531a84cac
eyjhb <eyjhbb@gmail.com>
2025-05-30 15:22:45 +0200

    changed how holidays provider works

--- calgen/models/VacanzaHolidays.py
+++ calgen/models/VacanzaHolidays.py
@@ -1,59 +1,92 @@
+from datetime import datetime
 import hashlib
 
 from calgen.models.Calendar import Calendar, CalendarTypes
 
 
-class VacanzaHolidays(Calendar):
-    """
-    Instantiate this calendar api class to further parse Holidays data
-    """
-
+class VacanzaHoliday(Calendar):
     def __init__(
         self,
-        data: dict = None,
-        year: int = None,
+        country: str,
+        holiday_date: datetime,
+        holiday_name: str,
+        all_subdivs: list[str],
+        map_subdiv_to_name: dict[str, str],
     ):
-        super(VacanzaHolidays, self).__init__(data, year)
+        super(VacanzaHoliday, self).__init__({}, holiday_date.year)
 
-        if data:
-            self.from_api(data)
+        # holiday information
+        self._country = country
+        self._date = holiday_date
+        self._name = holiday_name
 
-    def handle_regional_labelling(
-        self,
-        name: str,
-        subdivs: list[tuple[str, str]],
-        subdivs_except: bool = False,
-    ):
+        # maps a subdiv e.g. Alaska to AK
+        self._map_subdiv_to_name: dict[str, str] = map_subdiv_to_name
+        self._all_subdivs: list[str] = all_subdivs
+
+        self._subdivs: list[str] = []
+
+    def add_subdiv(self, subdiv: str):
+        if not subdiv:
+            return
+
+        self._subdivs.append(subdiv)
+
+    def get_calendar_type(self) -> CalendarTypes:
+        if len(self._all_subdivs) == len(self._subdivs) or len(self._subdivs) == 0:
+            return CalendarTypes.NATIONAL
+
+        return CalendarTypes.LOCAL
+
+    def regional_label(self):
         """Handle regional naming for holidays that fall under the CalendarTypes.LOCAL"""
-        # there is no subdivs -> everywhere
-        if not len(subdivs):
-            return name
 
-        # this should really be localised, but uncertain how to do this best
-        subdivs_except_text = ""
-        if subdivs_except:
-            subdivs_except_text = "All except "
+        # national holiday -> just the name
+        if self.get_calendar_type() == CalendarTypes.NATIONAL:
+            return self._name
 
+        # keep track of our subdivs, needed when we use the excluded (except text)
+        subdivs = self._subdivs
+        excluded_text: str = ""
+
+        # check if we can shorten the list, e.g. the length
+        # of subdivs is bigger than half of the TOTAL subdivs
+        if len(subdivs) >= len(self._all_subdivs) / 2:
+            excluded_subdivs: list[str] = []
+            for subdiv in self._all_subdivs:
+                if subdiv in subdivs:
+                    continue
+
+                excluded_subdivs.append(subdiv)
+
+            # insert the excluded text here
+            # it should be done in a better way than this...
+            excluded_text = "All except "
+            subdivs = excluded_subdivs
+
+        # only one subdiv, use the real name, not alias
         if len(subdivs) == 1:
-            return f"{name} ({subdivs_except_text}{subdivs[0][1]})"
+            subdiv = subdivs[0]
+            subdivs = [self._map_subdiv_to_name.get(subdiv, subdiv)]
 
-        subdivs_concat = ", ".join([v[0] for v in subdivs])
-
-        return f"{name} ({subdivs_except_text}{subdivs_concat})"
+        # concat and return
+        concat_subdivs = ", ".join(subdivs)
+        return f"{self._name} ({excluded_text}{concat_subdivs})"
 
     def from_api(self, data: dict):
-        date = data.get("date")
-        name = data.get("name")
-        subdivs = data.get("subdivs")
-        subdivs_except = data.get("subdivs_except")
-
         # generate unique_id. There could be a better way, but this should suffice
-        subdivs_hash_concat = ", ".join([v[0] for v in subdivs])
-        unique_str = data.get("country") + str(date) + name + str(self.year) + subdivs_hash_concat
+        subdivs_hash_concat = ", ".join([v for v in self._subdivs])
+        unique_str = (
+            self._country
+            + str(self._date)
+            + self._name
+            + str(self.year)
+            + subdivs_hash_concat
+        )
         self.unique_id = hashlib.sha1(unique_str.encode("utf8")).hexdigest()
 
         # rest of data
-        self.iso_date = date
-        self.name = self.handle_regional_labelling(name, subdivs, subdivs_except)
+        self.iso_date = self._date
+        self.name = self.regional_label()
         self.description = self.name
-        self.calendar_type = data.get("calendar_type")
+        self.calendar_type = self.get_calendar_type()
--- calgen/providers/VacanzaHolidaysProvider.py
+++ calgen/providers/VacanzaHolidaysProvider.py
@@ -1,5 +1,4 @@
-from calgen.models.Calendar import Calendar, CalendarTypes
-from calgen.models.VacanzaHolidays import VacanzaHolidays
+from calgen.models.VacanzaHolidays import VacanzaHoliday
 
 from calgen.providers.Provider import Provider
 
@@ -26,22 +25,19 @@ class VacanzaHolidaysProvider(Provider):
         try:
             cal = holidays.country_holidays(country, years=year, language=language)
         except NotImplementedError:
-            print(f"no calendar available for {country} with language {language}")
+            print(
+                f"no calendar available for {country} with language {language} (year {year})"
+            )
             return []
 
         # all subdivisions for this country
         subdivs = cal.subdivisions
 
-        # get the names for all the subdivisions
-        subdiv_to_name = {v: k for k, v in cal.subdivisions_aliases.items()}
-
         # handle cases with no subdivs
         if len(subdivs) == 0:
             subdivs = [False]
-            subdiv_to_name[False] = ""
 
-        # hold all holidays, including which divisions has it
-        holidays_with_subdivs: dict = {}
+        holidays_hashmap: dict[int, VacanzaHoliday] = {}
         for subdiv in subdivs:
             tcal = holidays.country_holidays(
                 country,
@@ -50,55 +46,28 @@ class VacanzaHolidaysProvider(Provider):
                 subdiv=subdiv,
                 categories=cal.supported_categories,
             )
-            for d, n in tcal.items():
-                k = hash(str(d) + n)
-                if k not in holidays_with_subdivs:
-                    holidays_with_subdivs[k] = {
-                        "country": country,
-                        "date": d,
-                        "name": n,
-                        "subdivs_except": False,
-                        "subdivs": [],
-                        "calendar_type": CalendarTypes.LOCAL,
-                    }
-                holidays_with_subdivs[k]["subdivs"].append(
-                    (subdiv, subdiv_to_name.get(subdiv, subdiv))
-                )
 
-        # if a holidays has ALL the subdivs, then it is a national holiday,
-        # and we remove ALL subdivs from it, otherwise it is local.
-        # if it as a local holiday, then we try to figure out if we can
-        # shorten it by saying "everywhere, EXCEPT these regions"
-        for k, holiday_with_subdivs in holidays_with_subdivs.items():
-            subdivs = holiday_with_subdivs["subdivs"]
-            if len(subdivs) == len(cal.subdivisions):
-                holidays_with_subdivs[k]["calendar_type"] = CalendarTypes.NATIONAL
-                holidays_with_subdivs[k]["subdivs"] = []
-                continue
+            map_subdiv_to_name = {v: k for k, v in cal.subdivisions_aliases.items()}
 
-            # unable to shorten it, just give up
-            if not len(subdivs) >= len(cal.subdivisions) / 2:
-                continue
+            for holiday_date, holiday_name in tcal.items():
+                k = hash(str(holiday_date) + holiday_name)
+                if k not in holidays_hashmap:
+                    holidays_hashmap[k] = VacanzaHoliday(
+                        country,
+                        holiday_date,
+                        holiday_name,
+                        subdivs,
+                        # cal.subdivisions_aliases,
+                        map_subdiv_to_name,
+                        # subdiv_to_name,
+                    )
 
-            # take out only teh aliases, so that we can filter it
-            subdivs_only_aliases = [v[0] for v in subdivs]
-            # filter out based on ALL the subdivisions available
-            except_subdiv_aliases = list(
-                filter(
-                    lambda subdiv: subdiv not in subdivs_only_aliases, cal.subdivisions
-                )
-            )
-
-            # generate the subdivs again, and say this is everywhere EXCEPT these
-            holidays_with_subdivs[k]["subdivs"] = [
-                (v, subdiv_to_name.get(v, v)) for v in except_subdiv_aliases
-            ]
-            holidays_with_subdivs[k]["subdivs_except"] = True
+                holidays_hashmap[k].add_subdiv(subdiv)
 
         return list(
             filter(
-                lambda x: x["calendar_type"].value == calendar_type,
-                holidays_with_subdivs.values(),
+                lambda x: x.get_calendar_type() == calendar_type,
+                holidays_hashmap.values(),
             )
         )
 
@@ -106,4 +75,8 @@ class VacanzaHolidaysProvider(Provider):
         """Utilises Holidays, builds, and returns a list of VacanzaHolidays models from the resulting data."""
         holidays = self.query(country, year, additional_options)
 
-        return [VacanzaHolidays(holiday, year) for holiday in holidays]
+        for h in holidays:
+            # no data, already included
+            h.from_api({})
+
+        return holidays
