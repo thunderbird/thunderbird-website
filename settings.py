56de9de27884740923805e4977af5ce8ba9bc4f5
Chris Aquino <chris@thunderbird.net>
2025-11-06 16:05:40 -0500

    Adds two copies of Nov 25 appeal content using Dec 24 styles

--- settings.py
+++ settings.py
@@ -360,6 +360,7 @@ UPDATES_CSS = {
     "appeal-nov24-style": ["less/appeals/nov24.less"],
     "appeal-dec24-style": ["less/appeals/dec24.less"],
     "appeal-apr25-style": ["less/appeals/apr25.less"],
+    "appeal-nov25-style": ["less/appeals/nov25.less"],
     "monthly-style": ["less/monthly.less"],
     'whatsnew-140': ['less/whatsnew-140.less'],
     "appeal-jun25-style": ["less/appeals/jun25.less"],
