e5bc46732bdf42257f074df1df0a63ffaf9a75d4
Chris Aquino <chris@thunderbird.net>
2026-01-26 10:15:54 -0500

    Update README, scripts, and Dockerfile to pull roadmaps content

--- pull-libs.sh
+++ pull-libs.sh
@@ -6,5 +6,7 @@ echo 'pulling product details'
 cd ../product-details && git pull
 echo 'pulling thunderbird notes'
 cd ../thunderbird_notes && git pull
+echo 'pulling roadmaps'
+cd libs/thunderbird_roadmaps && git pull
 
 echo 'done'
