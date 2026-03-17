73f2a1bb47585d809d305c9a45ecfef99a9d8371
Andrei Hajdukewycz <sancus@off.net>
2026-03-17 18:23:38 +0000

    Fix http-mx and decommission broker.

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -8,12 +8,6 @@ echo "Starting Thunderbird Website Container..."
 ln -sf /dev/stdout /var/log/httpd/access_log
 ln -sf /dev/stderr /var/log/httpd/error_log
 
-# Configure broker API key if provided via environment variable
-if [ -n "$MAILFENCE_APIKEY" ]; then
-    echo "Configuring broker with Mailfence API key..."
-    sed -i "s/^apikey = .*/apikey = '\&api-key=$MAILFENCE_APIKEY'/" /var/www/services/broker/settings.py
-fi
-
 # Remove stale Apache pid file if it exists (from previous run)
 rm -f /var/run/httpd/httpd.pid
 
