4c4bd4c84869edabce5877763c326e335297dc7e
Davi Nakano <davinakanoca@gmail.com>
2026-07-14 16:13:27 -0600

    Remove /thundermail page (Fixes #1257)

--- tests/test_webserver.py
+++ tests/test_webserver.py
@@ -89,7 +89,6 @@ TEST_CASES = [
     ("tb.pro", "/", 302, "/en-US/"),
     ("tb.pro", "/appointment", 302, "/en-US/appointment"),
     ("tb.pro", "/send", 302, "/en-US/send"),
-    ("tb.pro", "/thundermail", 302, "/en-US/thundermail"),
     ("tb.pro", "/waitlist", 302, "/en-US/waitlist"),
 
     # support.thunderbird.net - generic url formatter redirect (issue #1135)
