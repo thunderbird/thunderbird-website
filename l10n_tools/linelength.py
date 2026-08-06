836ac88c5dcf7351e5f98b0859f6d93cfb64e32a
Chris Aquino <chris@thunderbird.net>
2026-08-06 15:02:28 -0400

    Use uv, stop on errors, format consistently

--- l10n_tools/linelength.py
+++ l10n_tools/linelength.py
@@ -1,17 +1,14 @@
-import os
+from pathlib import Path
+
 import polib
-import sys
-os.chdir('..')
-sys.path.append('.')
-import settings
 
 
 # This is a ridiculous hack to get line lengths back to what Pontoon expects,
 # which avoids messy git diffs with unnecessary line length changes all over
 # the .po files.
 
-for lang in settings.PROD_LANGUAGES:
-    if lang != 'en-US':
-        popath = f'{settings.LOCALE_PATH}/{lang.replace("-", "_")}/LC_MESSAGES/messages.po'
-        po = polib.pofile(popath, wrapwidth=200)
-        po.save()
+locale_root = Path(__file__).resolve().parents[1] / "libs" / "locale"
+
+for po_path in sorted(locale_root.glob("*/LC_MESSAGES/messages.po")):
+    po = polib.pofile(po_path, wrapwidth=200)
+    po.save()
